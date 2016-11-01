bd4311194235dacb7ffa762385afa1a275ea57cf
Sajib <sajib@appscode.com>
2016-11-01 15:49:44 +0600

    Initial commits (coppied from Travisci-Laravel2)

---
+++ tests/ExampleTest.php
@@ -0,0 +1,25 @@
+<?php
+
+class ExampleTest extends TestCase {
+
+	/**
+	 * A basic functional test example.
+	 *
+	 * @return void
+	 */
+	public function testBasicExample()
+	{
+		$response = $this->call('GET', '/');
+
+		$this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals(200, $response->getStatusCode());
+	}
+
+}
+
